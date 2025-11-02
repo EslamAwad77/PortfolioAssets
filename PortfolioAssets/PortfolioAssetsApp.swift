4a866f4fd92468df6850b665f864a964b6b74920
EslamAwad <93745044+EslamAwad77@users.noreply.github.com>
2025-11-02 22:53:35 +0200

    Initial Commit

---
+++ PortfolioAssets/PortfolioAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAssetsApp.swift
+//  PortfolioAssets
+//
+//  Created by itroots on 02/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
